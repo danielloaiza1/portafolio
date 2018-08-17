b6b1f12511983682d060961c586259cc08d2551e
Daniel Loaiza <danielloaizad07@gmail.com>
2018-08-17 17:18:36 -0500

    Fin de la sección 7

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Producto } from '../interfaces/producto.interface';
+import { resolve } from 'path';
 
 @Injectable({
   providedIn: 'root'
@@ -9,22 +10,50 @@ export class ProductosService {
 
   cargando = true;
   productos: Producto[] = [];
+  productosFiltrado: Producto[] = [];
 
   constructor( private http: HttpClient) {
     this.cargarProductos();
   }
 
   private cargarProductos() {
-    this.http.get('https://portafolio-2f154.firebaseio.com/productos_idx.json')
-    .subscribe( (resp: Producto[]) => {
 
-      this.productos = resp;
-      this.cargando = false;
-      /*
-      setTimeout(() => {
+    return new Promise( (resolve, reject) => {
+      this.http.get('https://portafolio-2f154.firebaseio.com/productos_idx.json')
+      .subscribe( (resp: Producto[]) => {
+
+        this.productos = resp;
         this.cargando = false;
-      }, 1000); */
+        resolve();
+      } );
+    });
 
-    } );
+  }
+
+  getProducto(id: String) {
+    return this.http.get(`https://portafolio-2f154.firebaseio.com/productos/${ id }.json`);
+  }
+
+  buscarProducto( termino: string) {
+
+    if ( this.productos.length === 0) {
+      this.cargarProductos().then(() => {
+        this.filtrarProductos( termino );
+      });
+    } else {
+      this.filtrarProductos( termino );
+    }
+
+  }
+
+  private filtrarProductos( termino: string) {
+    termino = termino.toLowerCase();
+    this.productosFiltrado = [];
+    this.productos.forEach( prod => {
+      const tituloLower = prod.titulo.toLowerCase();
+      if ( prod.categoria.indexOf( termino ) >= 0 || tituloLower.indexOf( termino ) >= 0 ) {
+        this.productosFiltrado.push( prod );
+      }
+    });
   }
 }
